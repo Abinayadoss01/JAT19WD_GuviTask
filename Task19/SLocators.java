5005ef4b8a76d33cda5bf9e15f3ebb6930c81edf
Abinayadoss01 <abinayait28@gmail.com>
2024-07-03 18:54:00 +0530

    Add files via upload

--- Task19/SLocators.java
+++ Task19/SLocators.java
@@ -1,14 +1,19 @@
 package SeleniumPrjct;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SLocators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		//Launch the browser
 				WebDriver driver = new ChromeDriver();
@@ -21,13 +26,13 @@ public class SLocators {
 				driver.manage().window().maximize();
 						
 				//adding implicit wait
-				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 				
 				//using id locators
-				driver.findElement(By.id("name")).sendKeys("Abinaya");
+				driver.findElement(By.id("name")).sendKeys("test");
 						
 				//using css selectors
-						driver.findElement(By.cssSelector("input#email")).sendKeys("abinayait28821@gmail.com");
+						driver.findElement(By.cssSelector("input#email")).sendKeys("abinayatest@gmail.com");
 						
 				//using xpath
 						driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Abinaya@01");
@@ -41,7 +46,13 @@ public class SLocators {
 				
 				//using tagname locator
 						String text = driver.findElement(By.tagName("h1")).getText();
+						driver.findElement(By.tagName("h1")).click();
 						System.out.println(text);
+						
+						File Srce= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+						File dest= new File("C:\\Users\\abina\\eclipse-workspace\\SeleniumProject\\src\\main\\java\\SeleniumPrjct\\OutPut.png");
+						FileUtils.copyFile(Srce, dest);
 	}
+		
 
 }
